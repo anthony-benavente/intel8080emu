e71becf39b25c1e201f237d8314dcd95a44c0eea
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-02-05 14:01:12 -0500

    Started organizing tests

--- tests/tests_intel8080.h
+++ tests/tests_intel8080.h
@@ -6,10 +6,9 @@
 class Intel8080TestSuite : public CxxTest::TestSuite {
 	Intel8080 cpu;
 public:
-	void setUp() {
-		cpu.loadProgram(getProgram("../res/invaders.rom"));
-	}
+
 	void test_getNextOp(void) {
+		cpu.loadProgram(getProgram("../res/invaders.rom"));
 		cpu.getNextOp();
 		cpu.getNextOp();
 		cpu.getNextOp();
@@ -25,21 +24,139 @@ public:
 		TS_ASSERT_EQUALS(cpu.getNextOp(), 0x00);
 		TS_ASSERT_EQUALS(cpu.getPc(), 0x8);
 		TS_ASSERT_EQUALS(cpu.getNextOp(), 0xf5);
+		for (int i = 0; i <= 0xffff; i++) {
+			cpu.memory[i] = 0;
+		}
 		cpu.pc = 0;
 	}
+
 	void test_decode() {
 	    TS_ASSERT(false);
 	}
+
 	void test_NOP() {
 		TS_ASSERT_EQUALS(cpu.sp, 0);
+		TS_ASSERT_EQUALS(cpu.pc, 0);
+		TS_ASSERT_EQUALS(cpu.inte, false);
+		TS_ASSERT_EQUALS(cpu.status, 0);
+		for (int i = 0; i < 8; i++) {
+			TS_ASSERT_EQUALS(cpu.reg[i], 0);
+		}
+
 		cpu.NOP();
+
+		// NOP should have done nothing
 		TS_ASSERT_EQUALS(cpu.sp, 0);
+		TS_ASSERT_EQUALS(cpu.pc, 0);
+		TS_ASSERT_EQUALS(cpu.inte, false);
+		TS_ASSERT_EQUALS(cpu.status, 0);
+		for (int i = 0; i < 8; i++) {
+			TS_ASSERT_EQUALS(cpu.reg[i], 0);
+		}
 	}
-	void test_SHLD_A16() {
+
+	/* -------------------------------------------- */
+	/*   MOVE LOAD STORE Instructions				*/
+	/* -------------------------------------------- */
+	void test_MOV() {
+		// Test moving register to register
+		// Test moving register to memory
+		// Test moving register to memory
+		TS_ASSERT(false);
+	}
+
+	void test_MVI() {
+		// Test moving immediate to memory at H,L pair
+		// Test moving immediate to register
+		TS_ASSERT(false);
+	}
+
+	void test_LXI() {
+		// Test load immediate to pair B,C
+		// Test load immediate to pair D,E
+		// Test load immediate to pair H,L
 	    TS_ASSERT(false);
 	}
-	void test_STA_A16() {
+
+	void test_STAX() {
+		// Test store accumulator into memory @ B,C
+		// Test store accumulator into memory @ D,E
 	    TS_ASSERT(false);
+	}
+
+	void test_LDAX() {
+		// Test load accumulator from memory @ B,C
+		// Test load accumulator from memory @ D,E
+		TS_ASSERT(false);
+	}
+
+	void test_STA() {
+		cpu.reg[7] = 0xae;
+		cpu.pc = 0x9990;
+		cpu.memory[0x9990] = 0xa0;
+		cpu.memory[0x9991] = 0xaa;
+		cpu.cycles = 0;
+
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa0], 0);
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa1], 0);
+		cpu.STA_A16();
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa0], 0x00);
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa1], 0xae);
+		TS_ASSERT_EQUALS(cpu.cycles, 13);
+
+		cpu.pc = 0;
+		cpu.reg[7] = 0;
+		cpu.memory[0x9990] = 0;
+		cpu.memory[0x9991] = 0;
+		cpu.memory[0xaaa0] = 0;
+		cpu.memory[0xaaa1] = 0;
+		cpu.cycles = 0;
+	}
+
+	void test_LDA() {
+		// Test loading memory @ [pc + 1:pc + 2] into acculumator
+	    TS_ASSERT(false);
+	}
+
+	void test_SHLD() {
+		cpu.reg[4] = 0xae;
+		cpu.reg[5] = 0x29;
+		cpu.pc = 0x9990;
+		cpu.memory[0x9990] = 0xa0;
+		cpu.memory[0x9991] = 0xaa;
+		cpu.cycles = 0;
+
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa0], 0);
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa1], 0);
+		cpu.SHLD_A16();
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa0], 0x29);
+		TS_ASSERT_EQUALS(cpu.memory[0xaaa1], 0xae);
+		TS_ASSERT_EQUALS(cpu.cycles, 16);
+
+		cpu.pc = 0;
+		cpu.reg[4] = 0;
+		cpu.reg[5] = 0;
+		cpu.memory[0x9990] = 0;
+		cpu.memory[0x9991] = 0;
+		cpu.cycles = 0;
+	}
+
+	void test_LHLD() {
+		// Load memory @ [pc + 1:pc + 2] -> L, H respectively
+	    TS_ASSERT(false);
+	}
+
+	void test_XCHG() {
+		// Swap pair D,E with H,L respectively
+	    TS_ASSERT(false);
+	}
+
+	/* -------------------------------------------- */
+	/* /MOVE LOAD STORE Instructions
+	/* -------------------------------------------- */
+
+	void test_LXI_SP() {
+		TS_ASSERT(false);
 	}
 	void test_RLC() {
 	    TS_ASSERT(false);
@@ -164,9 +281,6 @@ public:
 	void test_IN() {
 	    TS_ASSERT(false);
 	}
-	void test_XCHG() {
-	    TS_ASSERT(false);
-	}
 	void test_EI() {
 	    TS_ASSERT(false);
 	}
@@ -189,24 +303,6 @@ public:
 	    TS_ASSERT(false);
 	}
 	void test_SBI() {
-	    TS_ASSERT(false);
-	}
-	void test_LXI_B() {
-	    TS_ASSERT(false);
-	}
-	void test_LXI_D() {
-	    TS_ASSERT(false);
-	}
-	void test_STAX_B() {
-	    TS_ASSERT(false);
-	}
-	void test_STAX_D() {
-	    TS_ASSERT(false);
-	}
-	void test_LXI_H() {
-	    TS_ASSERT(false);
-	}
-	void test_LXI_SP() {
 	    TS_ASSERT(false);
 	}
 	void test_INX_B() {
@@ -245,18 +341,6 @@ public:
 	void test_DCR_M() {
 	    TS_ASSERT(false);
 	}
-	void test_MVI_B() {
-	    TS_ASSERT(false);
-	}
-	void test_MVI_D() {
-	    TS_ASSERT(false);
-	}
-	void test_MVI_H() {
-	    TS_ASSERT(false);
-	}
-	void test_MVI_M() {
-	    TS_ASSERT(false);
-	}
 	void test_DAD_B() {
 	    TS_ASSERT(false);
 	}
@@ -267,18 +351,6 @@ public:
 	    TS_ASSERT(false);
 	}
 	void test_DAD_SP() {
-	    TS_ASSERT(false);
-	}
-	void test_LDAX_B() {
-	    TS_ASSERT(false);
-	}
-	void test_LDAX_D() {
-	    TS_ASSERT(false);
-	}
-	void test_LHLD() {
-	    TS_ASSERT(false);
-	}
-	void test_LDA() {
 	    TS_ASSERT(false);
 	}
 	void test_DCX_B() {
@@ -315,18 +387,6 @@ public:
 	    TS_ASSERT(false);
 	}
 	void test_DCR_A() {
-	    TS_ASSERT(false);
-	}
-	void test_MVI_C() {
-	    TS_ASSERT(false);
-	}
-	void test_MVI_E() {
-	    TS_ASSERT(false);
-	}
-	void test_MVI_L() {
-	    TS_ASSERT(false);
-	}
-	void test_MVI_A() {
 	    TS_ASSERT(false);
 	}
 	void test_POP_B() {
@@ -375,9 +435,6 @@ public:
 	    TS_ASSERT(false);
 	}
 	void test_RST7() {
-	    TS_ASSERT(false);
-	}
-	void test_MOV() {
 	    TS_ASSERT(false);
 	}
 	void test_ADD() {
