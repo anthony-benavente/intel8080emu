05a6321ad0720802d98f45d08b385669959c1173
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-02-25 01:36:07 -0500

    Added key map that can be checked to test if keys down/up

--- src/machine/machine.cpp
+++ src/machine/machine.cpp
@@ -2,13 +2,54 @@
 
 #include <SDL2/SDL.h>
 
-void Machine::start() {
+void Machine::start() { 
 	screen.initSDL();
 
 	SDL_Event e;
+    
+    keys.insert(std::pair<std::string, bool>("w", false));
+    keys.insert(std::pair<std::string, bool>("g", false));
+    keys.insert(std::pair<std::string, bool>("h", false));
+    keys.insert(std::pair<std::string, bool>("a", false));
+    keys.insert(std::pair<std::string, bool>("s", false));
+    keys.insert(std::pair<std::string, bool>("d", false));
+    keys.insert(std::pair<std::string, bool>("up", false));
+    keys.insert(std::pair<std::string, bool>("left", false));
+    keys.insert(std::pair<std::string, bool>("down", false));
+    keys.insert(std::pair<std::string, bool>("right", false));
+    
 	while(!quit) {
 		while (SDL_PollEvent(&e) != 0) {
-			quit = e.type == SDL_QUIT;
+            if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
+                switch (e.key.keysym.sym) {
+                    case SDLK_w:
+                        keys["w"] = e.type == SDL_KEYDOWN;
+                        break;
+                    case SDLK_a:
+                        keys["a"] = e.type == SDL_KEYDOWN;
+                        break;
+                    case SDLK_s:
+                        keys["s"] = e.type == SDL_KEYDOWN;
+                        break;
+                    case SDLK_d:
+                        keys["d"] = e.type == SDL_KEYDOWN;
+                        break;
+                    case SDLK_UP:
+                        keys["up"] = e.type == SDL_KEYDOWN;
+                        break;
+                    case SDLK_LEFT:
+                        keys["left"] = e.type == SDL_KEYDOWN;
+                        break;
+                    case SDLK_DOWN:
+                        keys["down"] = e.type == SDL_KEYDOWN;
+                        break;
+                    case SDLK_RIGHT:
+                        keys["right"] = e.type == SDL_KEYDOWN;
+                        break;
+                }   
+            } else if (e.type == SDL_QUIT) {
+			    quit = e.type == SDL_QUIT;
+            }
 		}
 		cycle();
 	}
